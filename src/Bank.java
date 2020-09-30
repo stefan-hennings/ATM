f29f0f6eecd39c688af8d8404f90ddadcfefa9ae
Stefan Hennings <stefan.hennings4@gmail.com>
2020-09-30 13:50:51 +0200

    Added double and string reading methods

--- src/Bank.java
+++ src/Bank.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 
 public class Bank {
     static Scanner in = new Scanner(System.in);
-
+    
     private static List<Customer> customerList = new ArrayList<>();
     private static List<Employee> employeeList = new LinkedList<>();
-
-
-    public static void welcomeMenu(){
+    
+    
+    public static void welcomeMenu() {
         String input;
         System.out.println("Välj vad du vill göra:\n" +
                 "1. Lägg till ny kund\n" +
@@ -18,140 +18,99 @@ public class Bank {
                 "3. Öppna konto\n" +
                 "4. Öppna lån\n" +
                 "5. Avsluta");
-
+        
         input = in.nextLine();
-
-        switch (input){
-            case "1"->
-                createCustomer();
-            case "2"->
-                createEmployee();
-
-            case "3"-> createLoan();
+        
+        switch (input) {
+            case "1" -> createCustomer();
+            case "2" -> createEmployee();
+            
+            case "3" -> createLoan();
 
 //            case "4":
 //                break;
 //            case "5":
         }
-
+        
     }
-
-    public static void createCustomer(){ //TODO: Felhantering, inga duplicates.
-
-        String name = null;
-        String personalNumber = null;
-
-        while(name==null){
-        System.out.print("Mata in kundens namn: ");
-        name = in.nextLine();
-        }
-
-        while (personalNumber == null) {
-            System.out.print("Mata in kundens personnummer: ");
-            personalNumber = in.nextLine();
-        }
+    
+    public static void createCustomer() { //TODO: Felhantering, inga duplicates.
+        
+        String name = getString("Mata in kundens namn: ");
+        
+        String personalNumber = getString("Mata in kundens personnummer: ");
         customerList.add(new Customer(name, personalNumber));
-
+        
     }
-
-    public static void testCode(){
+    
+    public static void testCode() {
         customerList.add(new Customer("Oscar", "x"));
         customerList.add(new Customer("Patrik", "x"));
         employeeList.add(new Employee("Stefan", "xx", 60000));
         employeeList.add(new Employee("Julia", "x", 25000));
-
+        
     }
-    public static void createEmployee(){
-        String name = null;
-        String personalNumber = null;
-        String salaryString = null;
-        double salary;
-
-        while(name==null){
-            System.out.print("Mata in den anställdas namn: ");
-            name = in.nextLine();
-        }
-
-        while (personalNumber == null) {
-            System.out.print("Mata in den anställdas personnummer: ");
-            personalNumber = in.nextLine();
-        }
-
-        while (salaryString == null || !tryParse(salaryString)) {
-            System.out.print("Mata in den anställdas lön: ");
-            salaryString = in.nextLine();
-            salaryString = salaryString.replace(',', '.');
-        }
-        salary = Double.parseDouble(salaryString);
-
+    
+    public static void createEmployee() {
+        String name = getString("Mata in den anställdas namn: ");
+        String personalNumber = getString("Mata in den anställdas personnummer: ");
+        double salary = Double.parseDouble("Mata in den anställdas lön: ");
+        
         employeeList.add(new Employee(name, personalNumber, salary));
-
+        
     }
-
-    public static void createLoan(){
-
-        String  customerPersonalNumber=null;
-        String customerName=null;
-        String employeePersonalNumber=null;
-        String interestString=null;
-        String loanAmountString=null;
-        double interest;
-        double loanAmount;
-
-        while (employeePersonalNumber==null) {
-            System.out.print("Ange ditt personnummer: ");
-            employeePersonalNumber = in.nextLine();
-        }
-        while(customerName==null) {
-            System.out.print("Ange kundens namn: ");
-            customerName = in.nextLine();
-        }
-        while (customerPersonalNumber==null) {
-            System.out.print("Ange kundens personnummer: ");
-            customerPersonalNumber = in.nextLine();
-        }
-        while (loanAmountString==null || !tryParse(loanAmountString)) {
-            System.out.print("Ange lånets storlek: ");
-            loanAmountString = in.nextLine();
-        }
-        loanAmount= Double.parseDouble(loanAmountString);
-        while (interestString==null || !tryParse(interestString)) {
-            System.out.print("Ange lånets räntesats: ");
-            interestString = in.nextLine();
-        }
-        interest = Double.parseDouble(interestString);
+    
+    public static void createLoan() {
+        String employeePersonalNumber = getString("Ange ditt personnummer: ");
+        String customerName = getString("Ange kundens namn: ");
+        String customerPersonalNumber = getString("Ange kundens personnummer: ");
+        
+        double loanAmount = getDouble("Ange lånets storlek: ");
+        double interest = getDouble("Ange lånets räntesats: ");
+        
         Customer c = getCustomer(customerName, customerPersonalNumber);
         Employee e = getEmployee(employeePersonalNumber);
-        Loan loan = new Loan(loanAmount, c, e, interest );
+        Loan loan = new Loan(loanAmount, c, e, interest);
         assert c != null;
         c.addLoan(loan);
-        System.out.println("Lån på " + c.getLoan().getDebt() +" skapat till " + c.getName());
+        System.out.println("Lån på " + c.getLoan().getDebt() + " skapat till " + c.getName());
     }
-
-    public static Customer getCustomer(String name, String personalNumber){
-        for(var c: customerList){
-            if(c.getName().equalsIgnoreCase(name.trim()) && c.getPersonalId().equalsIgnoreCase(personalNumber.trim())){
+    
+    private static String getString(String question) {
+        String response = null;
+        while (response == null || response.isEmpty()) {
+            System.out.print(question);
+            response = in.nextLine();
+        }
+        return response;
+    }
+    
+    private static Double getDouble(String question) {
+        while (true) {
+            try {
+                System.out.println(question);
+                return Double.parseDouble(in.nextLine().replace(',', '.'));
+            } catch (NumberFormatException e) {
+                System.out.println("Ogiltigt värde! ");
+            }
+        }
+    }
+    
+    public static Customer getCustomer(String name, String personalNumber) {
+        for (var c : customerList) {
+            if (c.getName().equalsIgnoreCase(name.trim()) && c.getPersonalId().equalsIgnoreCase(personalNumber.trim())) {
                 return c;
             }
         }
         return null;
     }
-    public static Employee getEmployee(String personalNumber){
-        for (var e: employeeList){
-            if(e.getPersonalId().equalsIgnoreCase(personalNumber)){
+    
+    public static Employee getEmployee(String personalNumber) {
+        for (var e : employeeList) {
+            if (e.getPersonalId().equalsIgnoreCase(personalNumber)) {
                 return e;
             }
         }
         return null;
-    }
-
-    public static boolean tryParse(String input){
-        try{
-            double parsed = Double.parseDouble(input);
-            return true;
-        } catch (Exception e){
-            System.out.println("Lönen måste bestå av siffror!!!");
-            return false;
-        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Bank.createCustomer();
         Bank.testCode();
         Bank.welcomeMenu();
 
