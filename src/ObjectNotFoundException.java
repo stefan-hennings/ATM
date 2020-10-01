bafe1d2b5d75946d73fd3e5afbe97a5167722856
stefan-hennings <stefan.hennings4@gmail.com>
2020-10-01 19:32:54 +0200

    Created ObjectNotFoundException

--- src/ObjectNotFoundException.java
+++ src/ObjectNotFoundException.java
@@ -1,13 +1,5 @@
 public class ObjectNotFoundException extends RuntimeException {
     /**
-     * Constructs a new runtime exception with {@code null} as its
-     * detail message.  The cause is not initialized, and may subsequently be
-     * initialized by a call to {@link #initCause}.
-     */
-    public ObjectNotFoundException() {
-    }
-    
-    /**
      * Constructs a new runtime exception with the specified detail message.
      * The cause is not initialized, and may subsequently be initialized by a
      * call to {@link #initCause}.
